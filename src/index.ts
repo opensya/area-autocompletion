ca950b5adb35bb24df555915ab4ae86362b9d9a7
Mamadou Talla DIA <ibntalla@gmail.com>
2025-11-25 01:45:19 +0000

    refactor: restructure search function to handle requests and improve readability

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,38 @@ import "dotenv/config";
 import { getDatabase } from "./db_client";
 import { AreaData } from "./types";
 
-export default async function search(query: string) {
-  const { db, client } = await getDatabase();
-  const collection = db.collection<AreaData>("area_data");
+export default {
+  async fetch(request: Request) {
+    const { db, client } = await getDatabase();
+    const collection = db.collection<AreaData>("area_data");
 
-  const results = await collection
-    .aggregate([
-      {
-        $match: { name: { $regex: query, $options: "i" } },
-      },
-      {
-        $lookup: {
-          from: "area_data",
-          localField: "parent",
-          foreignField: "code",
-          as: "parent_doc", // résultat mis dans ce tableau
+    const url = new URL(request.url);
+    const query = url.searchParams.get("query");
+
+    const results = await collection
+      .aggregate([
+        {
+          $match: { name: { $regex: query, $options: "i" } },
         },
-      },
-      {
-        $addFields: {
-          _parent: { $arrayElemAt: ["$parent_doc", 0] }, // récupérer un seul parent
+        {
+          $lookup: {
+            from: "area_data",
+            localField: "parent",
+            foreignField: "code",
+            as: "parent_doc", // résultat mis dans ce tableau
+          },
         },
-      },
-      { $project: { parent_doc: 0 } }, // supprimer le tableau temporaire
-    ])
-    .toArray();
+        {
+          $addFields: {
+            _parent: { $arrayElemAt: ["$parent_doc", 0] }, // récupérer un seul parent
+          },
+        },
+        { $project: { parent_doc: 0 } }, // supprimer le tableau temporaire
+      ])
+      .toArray();
 
-  await client.close();
+    await client.close();
 
-  return results;
-}
+    return results;
+  },
+};
